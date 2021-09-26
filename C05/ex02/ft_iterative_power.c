2520100ec23b22219f04fa4faf13aef77dd47114
박준영 <ib0712@naver.com>
2021-09-26 23:45:32 +0900

    add \ git commit -m add ;

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -15,10 +15,10 @@ int	ft_iterative_power(int nb, int power)
 	int	i;
 	int	result;
 
-	if (nb < 0 || power < 0)
-		return (0);
 	if (power == 0)
 		return (1);
+	if (nb == 0 || power < 0)
+		return (0);
 	i = 1;
 	result = 1;
 	while (i <= power)
